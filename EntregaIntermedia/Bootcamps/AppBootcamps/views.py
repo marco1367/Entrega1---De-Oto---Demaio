aa101535545e1c9be502ecd2cd587b664a3384f6
marco <marco.demaio@corebiz.ag>
2022-10-12 06:12:02 -0300

    add cursoForm - POST

--- EntregaIntermedia/Bootcamps/AppBootcamps/views.py
+++ EntregaIntermedia/Bootcamps/AppBootcamps/views.py
@@ -1,21 +1,41 @@
+import imp
 from django.shortcuts import render
-from AppBootcamps.views import *
+from AppBootcamps.models import *
+from AppBootcamps.forms import *
 
 # Create your views here.
 def inicio(request):
     return render(request, "AppBootcamps/inicio.html")
 
 
+
 def cursos(request):
-    return render(request, "AppBootcamps/cursos.html")
+    if request.method == "POST":
+        
+        miFormulario = CursoFormulario(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid:
+            info = miFormulario.cleaned_data
+            curso = Curso(nombre=info['nombre'], camada=info['camada'])
+            curso.save()
+            return render(request, "AppBootcamps/inicio.html")
+    else:
+        miFormulario = CursoFormulario()
+    return render(request, "AppBootcamps/cursos.html", {"miFormulario": miFormulario})
+
+
 
 
 def entregables(request):
     return render(request, "AppBootcamps/entregables.html")
 
 
+
+
 def estudiantes(request):
     return render(request, "AppBootcamps/estudiantes.html")
+
+
 
 
 def profesores(request):
